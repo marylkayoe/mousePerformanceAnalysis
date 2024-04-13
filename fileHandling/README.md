157a62cf744c01014b1fc4a113907e7a4cb1582f
Yoe <46240707+marylkayoe@users.noreply.github.com>
2024-04-13 20:48:48 +0900

    Starting to work on treadmill video processing functions
    neatening up video reading functions

---
+++ fileHandling/README.md
@@ -0,0 +1,2 @@
+This folder contains functions related to video file handling, such as
+converting AVI files to MP4 format using FFmpeg, downsampling, cropping, and such
--- videoProcessing/readVideoIntoMatrix.m
+++ videoProcessing/readVideoIntoMatrix.m
@@ -1,43 +1,95 @@
-function [videoMatrix frameRate] = readVideoIntoMatrix(filePath)
-% reads a .mp4 file in and returns it as an uint matrix (grayscale)
-% Create a VideoReader object
-if iscell(filePath)
-    filePath = filePath{1};
-end
-vidObj = VideoReader(filePath);
-disp(['reading file into matrix... ' filePath]);
+function [videoMatrix frameRate] = readVideoIntoMatrix(filePath, varargin)
+    % reads a .mp4 file in and returns it as an uint matrix (grayscale)
+    % filePath: path to the video file
 
-% Preallocate a 3D matrix to hold the video frames
-vidHeight = vidObj.Height;
-vidWidth = vidObj.Width;
-numFrames = vidObj.NumFrames;
-frameRate = vidObj.FrameRate;
+    % Optional input arguments:
+    %  'scaleFactor': a scalar value to scale the video by. Default is 1. scaleFactor < 1 will reduce the video size
+    %  'enhanceContrast': a boolean value to enhance the contrast of the video. Default is false
 
-videoMatrix = uint8(nan(vidHeight, vidWidth, numFrames));
+    % parse the input arguments
+    p = inputParser;
+    addRequired(p, 'filePath', @ischar);
+    addParameter(p, 'scaleFactor', 1, @isnumeric);
+    addParameter(p, 'enhanceContrast', false, @islogical);
+    parse(p, filePath, varargin{:});
 
-% Check if the video is monochrome or colored
-isMonochrome = false;
-if hasFrame(vidObj)
-    testFrame = readFrame(vidObj);
-    if size(testFrame,3) == 1
-        isMonochrome = true;
+    filePath = p.Results.filePath;
+    scaleFactor = p.Results.scaleFactor;
+    enhanceContrast = p.Results.enhanceContrast;
+
+    % Create a VideoReader object
+    if iscell(filePath)
+        filePath = filePath{1};
     end
-    vidObj.CurrentTime = 0; % Reset the video reader to the beginning
-end
 
-% Read each frame into the matrix
-k = 1;
-while hasFrame(vidObj)
-    frame = readFrame(vidObj);
-    if ~isMonochrome
-        frame = rgb2gray(frame);
+    videoMatrix = [];
+    frameRate = [];
+
+    % check that the file is .mp4
+    [~, ~, ext] = fileparts(filePath);
+
+    if ~strcmp(ext, '.mp4')
+        warning('File must be a .mp4 file, aborting...');
+        return;
     end
-    % Apply histogram equalization to increase contrast
-    enhancedFrame = histeq(frame);
-    videoMatrix(:,:,k) = frame;
-    k = k + 1;
+
+    % Check if the file exists
+    if ~exist(filePath, 'file')
+        warningMessage = sprintf('File does not exist:\n%s', filePath);
+        warning(warningMessage);
+        return;
+    end
+
+    % vidObj is a VideoReader object that handles the reading of the video file
+    vidObj = VideoReader(filePath);
+    disp(['reading file into matrix... ' filePath]);
+
+    % Preallocate a 3D matrix to hold the video frames
+    vidHeight = vidObj.Height;
+    vidWidth = vidObj.Width;
+    numFrames = vidObj.NumFrames;
+    frameRate = vidObj.FrameRate;
+    videoMatrix = zeros(vidHeight, vidWidth, numFrames, 'uint8');
+
+    % Check if the video is monochrome or colored
+    isMonochrome = false;
+    if hasFrame(vidObj)
+        testFrame = readFrame(vidObj);
+
+        if size(testFrame, 3) == 1
+            isMonochrome = true;
+        end
+
+        vidObj.CurrentTime = 0; % Reset the video reader to the beginning
+    end
+
+    % Read each frame into the matrix
+    frameIndex = 1;
+
+    while hasFrame(vidObj)
+        frame = readFrame(vidObj);
+
+        if ~isMonochrome
+            % remove blue color channel
+            %frame(:, :, 3) = zeros(size(frame, 1), size(frame, 2));
+            %convert to grayscale
+            frame = rgb2gray(frame);
+        end
+
+        % Apply histogram equalization to increase contrast if requested
+        if enhanceContrast
+            frame = histeq(frame);
+        end
+
+        % place the frame in the video matrix
+        videoMatrix(:, :, frameIndex) = frame;
+        frameIndex = frameIndex + 1;
+    end
+
+    % downscale the video in xy dimension if needed (no changes in the frame rate)
+    if scaleFactor < 1
+        disp(['downscaling video by factor ' num2str(scaleFactor)]);
+        videoMatrix = imresize(videoMatrix, scaleFactor);
+    end
+
 end
-end
-
-
-
